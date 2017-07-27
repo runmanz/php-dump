e6adad53ba557de1967efec6c8267b7040c30a2e
runmanz <296413715@qq.com>
2017-07-27 12:48:34 +0800

    deep dump

---
+++ src/Debug.php
@@ -0,0 +1,40 @@
+<?php
+namespace runmanz\Tools;
+
+
+class Debug
+{
+    public static function DeepDump($arr,$deep = 0){
+        echo '<pre style="margin: 5px auto">';
+        if(is_array($arr)){
+            $c = count($arr);
+            $space = '';
+            for ($i=0;$i<$deep;$i++){
+                $space .= '    ';
+            }
+            $deep++;
+            echo $space.'<b>array</b><i>(size='.$c.')</i><br>';
+            foreach ($arr as $k => $v){
+                $types = gettype($k);
+                if($types == 'integer')
+                    echo $space.'  '.$k.' => ';
+                else
+                    echo $space."  '".$k."' => ";
+                if(is_array($v)){
+                    static::DeepDump($v,$deep);
+                }else{
+                    $types = gettype($v);
+                    if($types == 'integer' || $types == 'int')
+                        echo "int ".$v."<br>";
+                    elseif($types == "NULL")
+                        echo "<i>".$types."</i><br>";
+                    else
+                        echo $types." '".$v."'<br>";
+                }
+            }
+        }else{
+            echo $arr;
+        }
+        echo '</pre>';
+    }
+}
